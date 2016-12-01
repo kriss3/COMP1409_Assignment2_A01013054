3b75629eceaf10d72847ed6512b9cad93d9d956a
kszczurowski <kszczurowski@aboslute.com>
2016-11-30 23:37:00 -0800

    Adding javaDoc to VehiclePurchase class

--- VehiclePurchase.java
+++ VehiclePurchase.java
@@ -31,26 +31,46 @@ public class VehiclePurchase
     }
     
     //properties
+    /**
+     * Public getter method to return a reference to a Customer object;
+     * @return returns a reference to customer object;
+     */
     public Customer getCustomer()
     {
         return this.customer;
     }
     
+    /**
+     * Public getter method to return a reference to purchaseDate object;
+     * @return returns a reference to PurchaseDate object;
+     */
     public PurchaseDate getPurchaseDate()
     {
         return this.purchaseDate;
     }
     
+    /**
+     * Public getter method to return a reference to a Vehicle object;
+     * @return returns a reference to vehicle object;
+     */
     public Vehicle getVehiclePurchased()
     {
         return this.vehiclePurchased;
     }
     
+    /**
+     * Public getter method to get the value of service package;
+     * @return returns value of service package as Boolean;
+     */
     public boolean getServicePackate()
     {
         return this.servicePackage;
     }
     
+    /**
+     * Public setter method to set the value of a service package;
+     * @param takes value parameter of Service Package as Boolean;
+     */
     public void setServicePackage(boolean value)
     {
         //asl Gary if we r interesting in recalculating profitMargin ans sellingPrive 
@@ -58,6 +78,11 @@ public class VehiclePurchase
         this.servicePackage = value;
     }   
     
+    /**
+     * Public method to calculate and set the value of vehicle's purchase price;<br>
+     * It does not return any value;<br>
+     * @param takes purchasePrice parameter as double; 
+     */
     public void calculatePurchasePrice(double purchasePrice)
     {
         if(!this.servicePackage)
@@ -70,6 +95,10 @@ public class VehiclePurchase
         }
     }
     
+    /**
+     * Public method to print out combined details of customer/vehicle/purchase date;<br>
+     * It does not return any value and it does not take any parameter;
+     */
     public void displayDetails()
     {
        System.out.println("Customer: " + customer.fullNameTag());
